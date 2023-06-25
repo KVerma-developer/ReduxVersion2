96f65ca21a638e96a438595574e30c7224e43505
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-06-25 20:29:11 +0530

    get value in ui with useSelector hook

--- src/components/features/icecream/IceCreamView.jsx
+++ src/components/features/icecream/IceCreamView.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 
 const IceCreamView = () => {
+  const numOfIceCream = useSelector((state)=> state.iceCream.numOfIceCream)
 
   return (
     <div>
-        <h2>Number of IceCream -</h2>
+        <h2>Number of IceCream - {numOfIceCream}</h2>
         <button>Order IceCream</button>
         <button>Restock IceCream</button>
       
