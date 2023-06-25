43747cfa7b2fd222a715ee6e403f69441e7cb005
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-06-25 21:58:53 +0530

    taking input from user for icecream restock

--- src/components/features/icecream/IceCreamView.jsx
+++ src/components/features/icecream/IceCreamView.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector , useDispatch } from 'react-redux';
 import { ordered ,restocked} from './icecreamSlice';
 
 const IceCreamView = () => {
+  const [value,setValue]=React.useState(0)
   const numOfIceCream = useSelector((state)=> state.iceCream.numOfIceCream)
   const dispatch = useDispatch()
 
   return (
     <div>
         <h2>Number of IceCream - {numOfIceCream}</h2>
+        <input value={value.toString()} type='number' onChange={e=>setValue(parseInt(e.target.value))}/>
         <button onClick={()=>dispatch(ordered())}>Order IceCream</button>
-        <button onClick={()=>dispatch(restocked(5))}>Restock IceCream</button>
+        <button onClick={()=>dispatch(restocked(value))}>Restock IceCream</button>
       
     </div>
   )
