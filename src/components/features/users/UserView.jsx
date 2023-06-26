b315195f583fcffc0af8eccdeed3c140ea2935ad
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-06-26 11:17:11 +0530

    user fetch successfully with error loading

--- src/components/features/users/UserView.jsx
+++ src/components/features/users/UserView.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { fetchUsers } from './userSlice'; // Corrected import statement
+
+import { useDispatch, useSelector } from 'react-redux';
 
 const UserView = () => {
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.user); //this is from store
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div>
-
-        <h2>List of Users</h2>
-      
+      <h2>List of Users</h2>
+      {user.loading && <div>Loading...</div>}
+      {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+      {!user.loading && user.users.length ? (
+        <ul>
+          {user.users.map((user) => (
+            <li key={user.name}>{user.name}</li>
+          ))}
+        </ul>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default UserView
+export default UserView;
+
